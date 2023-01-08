b6eb957e5af94bf733fd9be6db24b8a364941efb
vernetto <Ernesto0z>
2023-01-08 18:54:49 +0100

    using router

--- src/main/ui/src/App.js
+++ src/main/ui/src/App.js
@@ -1,28 +1,42 @@
-import './App.css';
-import RideTable from './RideTable'
+import "./App.css";
+import RideTable from "./RideTable";
 
 import React from "react";
-import {Link, Routes,  Route} from "react-router-dom";
-import SweetHome from './SweetHome';
-import AddUser from './AddUser';
+import { Link, Routes, Route } from "react-router-dom";
+import AddUser from "./AddUser";
+import AddCity from "./AddCity";
+import AddRide from "./AddRide";
+import Ride from "./Ride";
 
 function App() {
   return (
     <>
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-    </ul>
-  </nav>
-  
-  <Routes>
-       <Route path="/" element={<RideTable/>}></Route>
-       <Route path="/adduser" element={<AddUser/>}></Route>
-  </Routes>
-  </>
-  )
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/addride">Add Ride</Link>
+          </li>
+          <li>
+            <Link to="/adduser">Add User</Link>
+          </li>
+          <li>
+            <Link to="/addcity">Add City</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<RideTable />}></Route>
+        <Route path="/adduser" element={<AddUser />}></Route>
+        <Route path="/addcity" element={<AddCity />}></Route>
+        <Route path="/addride" element={<AddRide />}></Route>
+        <Route path="/ride/:id" element={<Ride />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
